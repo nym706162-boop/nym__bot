b90dfb7ae6a3d9f2f1fcd02e5bf5d3fbf310dec1
nym706162-boop <nym706162@gmail.com>
2026-09-01 20:46:34 +0530

    Update _inline.py
    Signed-off-by: nym706162-boop <nym706162@gmail.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
+
 from pyrogram import types
 
 from anony import app, config, lang
@@ -36,11 +37,11 @@ class Inline:
         if not remove:
             keyboard.append(
                 [
-                    self.ikb(text="▶️", callback_data=f"controls resume {chat_id}"),
-                    self.ikb(text="⏸️", callback_data=f"controls pause {chat_id}"),
-                    self.ikb(text="🔄", callback_data=f"controls replay {chat_id}"),
-                    self.ikb(text="⏭️", callback_data=f"controls skip {chat_id}"),
-                    self.ikb(text="⏹️", callback_data=f"controls stop {chat_id}"),
+                    self.ikb(text="▷", callback_data=f"controls resume {chat_id}"),
+                    self.ikb(text="II", callback_data=f"controls pause {chat_id}"),
+                    self.ikb(text="⥁", callback_data=f"controls replay {chat_id}"),
+                    self.ikb(text="‣‣I", callback_data=f"controls skip {chat_id}"),
+                    self.ikb(text="▢", callback_data=f"controls stop {chat_id}"),
                 ]
             )
         return self.ikm(keyboard)
