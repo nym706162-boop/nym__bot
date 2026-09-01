8ef6d1bde3cb09699cc415fc1d6aabc7ed1dd8cb
nym706162-boop <nym706162@gmail.com>
2026-09-01 19:49:24 +0530

    Update _inline.py
    Signed-off-by: nym706162-boop <nym706162@gmail.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -37,11 +37,11 @@ class Inline:
         if not remove:
             keyboard.append(
                 [
-                    self.ikb(text="▷", callback_data=f"controls resume {chat_id}"),
-                    self.ikb(text="II", callback_data=f"controls pause {chat_id}"),
-                    self.ikb(text="⥁", callback_data=f"controls replay {chat_id}"),
-                    self.ikb(text="‣‣I", callback_data=f"controls skip {chat_id}"),
-                    self.ikb(text="▢", callback_data=f"controls stop {chat_id}"),
+                    self.ikb(text="▶️", callback_data=f"controls resume {chat_id}"),
+                    self.ikb(text="⏸️", callback_data=f"controls pause {chat_id}"),
+                    self.ikb(text="🔄", callback_data=f"controls replay {chat_id}"),
+                    self.ikb(text="⏭️", callback_data=f"controls skip {chat_id}"),
+                    self.ikb(text="⏹️", callback_data=f"controls stop {chat_id}"),
                 ]
             )
         return self.ikm(keyboard)
