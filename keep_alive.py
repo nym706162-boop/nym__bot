52ccbf5c734134f44aeae27e0256fc2903d7381b
nym706162-boop <nym706162@gmail.com>
2026-09-10 21:18:21 +0530

    Update keep_alive.py
    Signed-off-by: nym706162-boop <nym706162@gmail.com>

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -14,9 +15,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
 def run():
-    server = HTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
     server.serve_forever()
 
 def keep_alive():
     t = threading.Thread(target=run)
+    t.daemon = True
     t.start()
